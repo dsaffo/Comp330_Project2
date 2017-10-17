1dd3dff9fc446fcfec8cc6c11e9eeee931cb428f
Jorge Ramirez <jramirez12@luc.edu>
2017-10-17 14:51:56 -0500

    Identifiers Test

--- app/src/test/java/com/brendan/notetakinghw2/ExampleUnitTest.java
+++ app/src/test/java/com/brendan/notetakinghw2/ExampleUnitTest.java
@@ -1,6 +1,7 @@
 package com.brendan.notetakinghw2;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import org.junit.Test;
@@ -64,10 +65,32 @@ public class ExampleUnitTest {
     String content3 = "This is a test for references ^reference";
 
     String title4 = "Friday meeting";
-    String content4 = "This is a test for multiple and equal identifiers !hello !hello";
+    String content4 = "This is a test for multiple and equal identifiers !hello";
 
-    String title5 = "Saturday meeting";
-    String content5 = "This is a test for multiple and different identifiers !hello !water";
+    String title5 = "Friday meeting";
+    String content5 = "This is a test for multiple and equal identifiers #hello #water";
+
+    String title6 = "Friday meeting";
+    String content6 = "This is a test for no identifiers !BALLOON";
+
+    String title7 = "Friday meeting";
+    String content7 = "This is a test for no identifiers !!LUC";
+
+    String title8 = "Saturday meeting";
+    String content8 = "This is a test for id at the end @";
+
+    String title9 = "Sunday meeting";
+    String content9 = "This is a test for double mentions at the end @@";
+
+    String title10 = "Monday meeting";
+    String content10 = "This is a test for double topics ##";
+
+    String title11 = "Tuesday meeting";
+    String content11 = "This is a test for double references ^^";
+
+
+
+
 
 
 
@@ -86,6 +109,25 @@ public class ExampleUnitTest {
 
     Note test5 = new Note(time, title5, content5, Utilities.sortMarks("@", content5), Utilities.sortMarks("#", content5), checkIdentifiersTest(list5, content5), Utilities.sortMarks("^", content5));
     ArrayList<Note> list6 = saveNewNote(test5, list5);
+
+    Note test6 = new Note(time, title6, content6, Utilities.sortMarks("@", content6), Utilities.sortMarks("#", content6), checkIdentifiersTest(list6, content6), Utilities.sortMarks("^", content6));
+    ArrayList<Note> list7 = saveNewNote(test6, list6);
+
+    Note test7 = new Note(time, title7, content7, Utilities.sortMarks("@", content7), Utilities.sortMarks("#", content7), checkIdentifiersTest(list7, content7), Utilities.sortMarks("^", content7));
+    ArrayList<Note> list8 = saveNewNote(test7, list7);
+
+    Note test8 = new Note(time, title8, content8, Utilities.sortMarks("@", content8), Utilities.sortMarks("#", content8), checkIdentifiersTest(list8, content8), Utilities.sortMarks("^", content8));
+    ArrayList<Note> list9 = saveNewNote(test8, list8);
+
+    Note test9 = new Note(time, title9, content9, Utilities.sortMarks("@", content9), Utilities.sortMarks("#", content9), checkIdentifiersTest(list9, content9), Utilities.sortMarks("^", content9));
+    ArrayList<Note> list10 = saveNewNote(test9, list9);
+
+    Note test10 = new Note(time, title10, content10, Utilities.sortMarks("@", content10), Utilities.sortMarks("#", content10), checkIdentifiersTest(list10, content10), Utilities.sortMarks("^", content10));
+    ArrayList<Note> list11 = saveNewNote(test10, list10);
+
+    Note test11 = new Note(time, title11, content11, Utilities.sortMarks("@", content11), Utilities.sortMarks("#", content11), checkIdentifiersTest(list11, content10), Utilities.sortMarks("^", content11));
+    ArrayList<Note> list12 = saveNewNote(test11, list11);
+
 
 
     @Test
@@ -109,12 +151,54 @@ public class ExampleUnitTest {
     }
 
     @Test
-    public void testingMultipleIdentifiers() throws Exception {
+    public void testingIdentifiers() throws Exception {
+        assertEquals(1, test4.getnIDs().size());
+
+    }
+
+
+    @Test
+    public void testGet() throws Exception {
         assertEquals(2, test5.getnIDs().size());
+
+    }
+    @Test
+    public void assertTrueID() throws Exception{
+        assertTrue(test5.getnIDs().size()== 1);
+    }
+
+    @Test
+    public void testingNoIdentifier() throws Exception {
+        assertEquals(0, test6.getnIDs().size());
+
+    }
+
+    @Test
+    public void testingDoubles() throws Exception {
+        assertEquals(0, test7.getnIDs().size());
+
+    } //TODO:not reading the identifiers if they're next to each other. Only reading the first id
+
+    @Test
+    public void testIDentifieratEnd() throws Exception {
+        assertEquals(1, test8.getnIDs().size());
+    }
+    @Test
+    public void testDoubleTopics() throws Exception {
+        assertEquals(1, test9.getnTopics().size());
+        assertEquals(1, test9.getnIDs().size());
+    }
+
+    public void testDoubleReference() throws Exception {
+        assertEquals(1, test10.getnRefs().size());
     }
 
 
-    }
+}
+
+
+
+
 
 
 
